801fd73ac2656180eba4926516afe97b83292c46
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-08-27 04:07:21 +0200

    disable ghc.environment file creation in cabal-install 2.2.0.0

---
+++ default.nix
@@ -0,0 +1,14 @@
+self: super: {
+
+# patch cabal-install to *not* create .ghc.environment.* files.
+
+# TODO if patching is getting complicated due to upstream changes in file, then
+# use sed...
+
+cabal-install = if (isNull (builtins.match "2.2.0.0" super.cabal-install.version))
+  then super.cabal-install
+  else super.cabal-install.overrideAttrs (oldAttrs: rec {
+          buildDepends = [ super.patch ];
+          patches = [ ./disable-ghc-env.patch ];
+        });
+}
